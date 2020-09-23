5c472fe32a25ecca80192acdd4e8827e36d39452
Agus Sumarna <sumarna.agus@gmail.com>
2020-09-23 16:19:50 +0700

    update config.php

--- infoniaga_application/config/config.php
+++ infoniaga_application/config/config.php
@@ -24,7 +24,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 |
 */
 
-$config['base_url']='http://localhost/webinfoniaga';
+$config['base_url']='http://localhost/infoniaga';
 $config['fonts_path']= 'assets/fonts/';
 
 /*
